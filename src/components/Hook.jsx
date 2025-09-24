0fd521935a84cd5fef8d1fda499efe69c862c607
APHIWAT-TECH <changsaoggo123@gmail.com>
2025-09-24 15:00:15 +0700

    add lazy

--- src/components/Hook.jsx
+++ src/components/Hook.jsx
@@ -1,15 +1,19 @@
 import ComingSoon from "./ComingSoon";
 import { Route } from "react-router-dom";
-import React,{lazy} from "react";
+import React, { lazy } from "react";
+
+
+const LazyComingSoon = (name) =>
+  lazy(() => Promise.resolve({ default: () => <ComingSoon name={name} /> }));
 
 export const hookmap = {
   usestate: () => lazy(() => import('./useStateEx')),//<ComingSoon name="useState" />,
   useeffect: () => lazy(() => import('./useEffectEx')),//<ComingSoon name="useState" />,
   usecontext: () => lazy(() => import('./useContextEx')),//<ComingSoon name="useState" />,
-  usereducer: () => <ComingSoon name="useReducer" />,
-  useref: () => <ComingSoon name="useRef" />,
-  usememo: () => <ComingSoon name="useMemo" />,
-  usecallback: () => <ComingSoon name="useCallback" />
+  usereducer: () => LazyComingSoon("useReducer"),
+  useref: () => LazyComingSoon("useRef"),
+  usememo: () => LazyComingSoon("useMemo"),
+  usecallback: () => LazyComingSoon("useCallback")
 };
 
 export function generateRoutes() {
